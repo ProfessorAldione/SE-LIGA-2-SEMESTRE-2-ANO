4282aa2455a12c6fbe0b825f6e13fa09548c081c
ProfessorAldione <aldione.costa@escola.pr.gov.br>
2025-11-30 21:09:50 -0300

    a

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Matemática I',
+    'O que eu aprendi em programação?',
+    'Aprendi o que é HTML, CSS e JAVA SCRIPT'
+)
+
+criaCartao(
+    'Matemática I',
+    'O que eu aprendi em programação?',
+    'A usar o Github.'
+)
+
+criaCartao(
+    'Matemática I',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Matemática I',
+    'Qual linguagem de programação achei mais fácil?',
+    'Python'
+)
